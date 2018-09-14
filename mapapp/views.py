8e782f53b487154d9a43f49c0385114d6a6a7f18
syk1k <dambemondo@gmail.com>
2018-09-14 09:16:34 -0500

    Route using response from osrm api

--- mapapp/views.py
+++ mapapp/views.py
@@ -6,22 +6,57 @@ from django.views import View
 
 
 class MapView(View):
+
+    def convert_longlat_latlong(self, coordinates):
+        return_coordinates = []
+        for coordinate in coordinates:
+            lat,long_ = coordinate[1], coordinate[0]
+            return_coordinates.append([lat, long_])
+        return return_coordinates
+
+
+
     def get(self, *args, **kwargs):
         return render(self.request, 'mapapp/routeform.html')
 
+
+    
     def post(self, *args, **kwargs):
+        context = {}
         try:
             from_ = self.request.POST['from']
+            #https://osmand.net/go?lat=6.1866007&lon=1.1579759&z=20
             to_ = self.request.POST['to']
-            result = requests.get(f'http://router.project-osrm.org/route/v1/driving/{from_};{to_}?exclude=motorway')
+            result = requests.get(f'http://router.project-osrm.org/route/v1/driving/{from_};{to_}?geometries=geojson')
             print(result)
+            context['result'] = result.json()
+            coordinates = result.json()['routes'][0]['geometry']['coordinates']
             distance = result.json()['routes'][0]['distance']
+
+            distance = distance/1000
+
+            distance_arrondie = int(distance)
+            print(distance_arrondie)
+            if distance_arrondie > distance:
+                distance_arrondie = distance_arrondie - 1
+            reste_distance = distance - distance_arrondie
+
+            if reste_distance >= 0.5:
+                distance_arrondie = distance_arrondie + 1
+            
+            prix = 100 * distance_arrondie
+
+            latlong = self.convert_longlat_latlong(coordinates)
+            print(latlong);
             context = {
                 'result':result.json(),
                 'from': from_,
                 'to': to_,
                 'distance': distance,
+                'distance_arrondie': distance_arrondie,
+                'latlong' : latlong,
+                'prix': prix,
             }
         except:
-            context = {}
+            print("Error")
         return render(self.request, 'mapapp/index.html', context)
--- static/js/app.js
+++ static/js/app.js
@@ -12,4 +12,46 @@
         layers: [osm]
     });
 
+
+    var redMarker = L.ExtraMarkers.icon({
+        icon: 'fa-coffee',
+        markerColor: 'red',
+        shape: 'circle',
+        prefix: 'fa'
+    });
+
+    var greenMarker = L.ExtraMarkers.icon({
+        icon: 'fa-coffee',
+        markerColor: 'green',
+        shape: 'circle',
+        prefix: 'fa'
+    });
+
+    try{
+        latlong = JSON.parse(latlong);
+        console.log(latlong);
+
+        from_ = latlong[0]
+        to_ = latlong[latlong.length-1]
+
+        from_marker = L.marker(from_, {icon: redMarker});
+        from_marker.addTo(map);
+        from_marker.bindPopup("From");
+        to_marker = L.marker(to_, {icon: greenMarker});
+        to_marker.addTo(map);
+        to_marker.bindPopup("To");
+
+       
+        
+
+        var polyline = L.polyline(latlong, {color: 'red'}).addTo(map);
+        // zoom the map to the polyline
+        map.fitBounds(polyline.getBounds());
+    }catch(e){
+
+    }
+    
+
+    
+
 })();
